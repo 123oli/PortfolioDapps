63be840e8a3b563239baf308253e997fec43355b
Ganesh oli <ganesh@thaiofficefurniture.com>
2023-01-22 11:06:03 +0545

    list app

---
+++ todolistdapp/todo_list/lib.rs
@@ -0,0 +1,113 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use ink_lang as ink;
+
+#[ink::contract]
+mod todo_list {
+    use ink_storage::{traits::{SpreadAllocate, PackedLayout, SpreadLayout}, Mapping};
+
+
+    pub type TodoItemId = i32;
+
+    // Item priorities enum
+    #[derive(scale::Decode, scale::Encode, PackedLayout, SpreadLayout)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum Prioritise {
+        HIGH,
+        LOW,
+        MEDIUM,
+    }
+
+    // item todo error
+    #[derive(scale::Decode, scale::Encode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum TodoError {
+        ItemNotExists
+    }
+
+    #[derive(scale::Decode, scale::Encode, PackedLayout, SpreadLayout)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub struct TodoItem {
+        owner: AccountId,
+        item_name: String,
+        is_completed: bool,
+        priority: Prioritise
+    }
+
+    // Events for toto item
+    #[ink(event)]
+    pub struct ItemCreated {
+        #[ink(topic)]
+        item: TodoItem,
+    }
+
+    #[ink(event)]
+    pub struct ItemUpdated {
+        #[ink(topic)]
+        item: TodoItem,
+    }
+
+    #[ink(storage)]
+    #[derive(SpreadAllocate)]
+    pub struct TodoList {
+        owner: AccountId,
+        item: Mapping<TodoItemId, TodoItem>,
+        item_id: i32,
+    }
+
+    impl TodoList {
+        #[ink(constructor)]
+        pub fn new() -> Self {
+            let owner = Self::env().caller();
+            ink_lang::utils::initialize_contract(|contract: &mut TodoList|{
+                contract.owner = owner;
+                contract.item = Mapping::default();
+                contract.item_id = 1;
+            })
+        }
+
+        #[ink(message)]
+        pub fn create_todo(&mut self, item_name: String, priority: Prioritise ) -> Result<(), TodoError> {
+            let caller = self.env().caller();
+            let item_id = self.get_item_id();
+
+            let item = TodoItem {
+                owner: caller,
+                item_name,
+                is_completed: false,
+                priority,
+            };
+
+            self.item.insert(item_id, &item);
+            self.env().emit_event(ItemCreated{item});
+            Ok(())
+        }
+
+        #[ink(message)]
+        pub fn update_item(&mut self, item_id: TodoItemId) -> Result<(), TodoError> {
+            let item = self.item.get(item_id);
+            let caller = self.env().caller();
+            match item {
+                Some(value) => {
+                    let item = TodoItem {
+                        owner: caller,
+                        item_name: value.item_name,
+                        is_completed: true,
+                        priority: value.priority,
+                    };
+                    self.item.insert(item_id, &item);
+                    self.env().emit_event(ItemUpdated{item});
+                },
+                None => return Err(TodoError::ItemNotExists),
+            }
+            Ok(())
+        }
+
+        // Item next Id
+        pub fn get_item_id(&mut self) -> TodoItemId {
+            let id = self.item_id;
+            self.item_id += 1;
+            id
+        }
+    }
+}
